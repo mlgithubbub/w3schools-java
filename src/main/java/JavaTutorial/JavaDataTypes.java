fe845e34ddc69fb5fa2c774c095da20eec4b9c90
mlgithubbub <to.morganlinn@gmail.com>
2023-06-21 15:53:21 +0200

    starts Java Tutorial

---
+++ src/main/java/JavaTutorial/JavaDataTypes.java
@@ -0,0 +1,4 @@
+package JavaTutorial;
+
+public class JavaDataTypes {
+}
---
+++ src/main/java/JavaTutorial/JavaOutput.java
@@ -0,0 +1,18 @@
+package JavaTutorial;
+
+public class JavaOutput {
+
+    public static void main(String[] args) {
+        //Print text
+        System.out.println("Hi!");
+        System.out.print("Hi!");
+
+        //Print numbers
+        System.out.println(8);
+
+        //Perform mathematical calculations
+        System.out.println(3+3);
+        System.out.println(2*5);
+    }
+
+}
